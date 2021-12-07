0837d09402bf22c404e29a678173706490eae13e
Erinaldo Prado <eri.prado@hotmail.com>
2021-12-07 11:19:05 -0300

    add

--- javascript/10 forms/js/script.js
+++ javascript/10 forms/js/script.js
@@ -1,16 +1,21 @@
-const contato = document.querySelector('#contato')
-const text = document.querySelector('#texto')
-
+const contato = document.querySelector("#contato");
+const text = document.querySelector("#texto");
+const inputa = document.querySelector("input[type='color']");
 
 function handleChange(event) {
-const target = event.target
-if(!target.checkValidity()) {
-  target.classList.add('invalido')
-  // Atribui ao proximo elemento depois do target (span) o valor da mensagem de validação pronta
-  target.nextElementSibling.innerText = target.validationMessage
-} else {
-  target.classList.remove('invalido')
-}
+  const target = event.target;
+  if (!target.checkValidity()) {
+    target.classList.add("invalido");
+    // Atribui ao proximo elemento depois do target (span) o valor da mensagem de validação pronta
+    target.nextElementSibling.innerText = target.validationMessage;
+  } else {
+    target.classList.remove("invalido");
+  }
 }
 
-contato.addEventListener('change', handleChange)
+function handleKeyUp(event) {
+  document.body.style.background = event.target.value;
+}
+
+contato.addEventListener("change", handleChange);
+inputa.addEventListener("change", handleKeyUp);
